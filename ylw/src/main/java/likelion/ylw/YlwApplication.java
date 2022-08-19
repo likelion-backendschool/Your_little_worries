ab8b406a21e9a81713e11d145bf1adb5f8e28698
DOSEOKYEONG <dho88dho@naver.com>
2022-08-19 17:37:28 +0900

    TEST: ignore 작동?

--- ylw/src/main/java/likelion/ylw/YlwApplication.java
+++ ylw/src/main/java/likelion/ylw/YlwApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class YlwApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(YlwApplication.class, args);
 	}
 
