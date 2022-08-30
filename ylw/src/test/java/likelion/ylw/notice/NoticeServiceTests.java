f90ca21bc478bb0fc85d7e65fa9fc48017573bc2
nunusmohum <halojw301@gmail.com>
2022-08-30 16:20:13 +0900

    git notice test ì 추가

--- ylw/src/test/java/likelion/ylw/notice/NoticeServiceTests.java
+++ ylw/src/test/java/likelion/ylw/notice/NoticeServiceTests.java
@@ -21,4 +21,13 @@ public class NoticeServiceTests {
         Member member = memberService.getMemberId("member2");
         noticeService.create("제목입니다.", "내용입니다.", member);
     }
+
+    @Test
+    @DisplayName("공지사항 100개 생성")
+    public void createNotice100() {
+        Member member = memberService.getMemberId("member2");
+        for (int i = 0; i < 100; i++){
+            noticeService.create(String.format("제목입니다%d", i), String.format("내용입니다%d", i), member);
+        }
+    }
 }
