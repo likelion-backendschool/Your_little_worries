2c12a91cc710ecbc11b682602ddd93f96bdbf70c
DOSEOKYEONG <dho88dho@naver.com>
2022-09-04 11:51:55 +0900

    feat:

--- ylw/src/main/java/likelion/ylw/article/ArticleItemRepository.java
+++ ylw/src/main/java/likelion/ylw/article/ArticleItemRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleItemRepository extends JpaRepository<ArticleItem, Integer> {
+    List<ArticleItem> findArticleItemByArticleId(Integer articleId);
 }
