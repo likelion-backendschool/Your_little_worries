94c76985c451e2da1e85de9581747d2647d7f5df
DOSEOKYEONG <dho88dho@naver.com>
2022-09-07 20:38:44 +0900

    feat : articlemodify

--- ylw/src/main/java/likelion/ylw/stats/StatsCollectionForm.java
+++ ylw/src/main/java/likelion/ylw/stats/StatsCollectionForm.java
@@ -10,12 +10,13 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class StatsCollectionForm {
 
+    @NotNull(message = "투표항목을 선택해주세요.")
     private Integer articleItemId;
 
     @NotNull(message = "나이를 입력해주세요.")
     private Integer age;
 
-    @NotEmpty(message = "성별을 입력해주세요.")
+    @NotEmpty(message = "성별을 골라주세요.")
     private String gender;
 
     private String userName;
