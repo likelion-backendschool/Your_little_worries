1d37720484d30045440fcdff73e150dba93a4d77
nunusmohum <halojw301@gmail.com>
2022-09-26 23:26:06 +0900

    style: 반응형 적용

--- src/main/java/likelion/ylw/notice/NoticeRepository.java
+++ src/main/java/likelion/ylw/notice/NoticeRepository.java
@@ -3,6 +3,7 @@ package likelion.ylw.notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,7 +11,7 @@ public interface NoticeRepository extends JpaRepository<Notice, Integer> {
 
     @Modifying
     @Query("update Notice p set p.viewCount = p.viewCount + 1 where p.id = :id")
-    int updateCount(Integer id);
+    int updateCount(@Param("id")Integer id);
 
     Optional<Notice> findTopByOrderByIdDesc();
 }
